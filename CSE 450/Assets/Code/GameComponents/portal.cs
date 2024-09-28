84f41cfe8dda480a37f57111ab26d120c9affce9
PerryJiang1 <j.pengyu@wustl.edu>
2024-09-28 01:04:21 -0500

    W5 Update

--- CSE 450/Assets/Code/GameComponents/portal.cs
+++ CSE 450/Assets/Code/GameComponents/portal.cs
@@ -4,26 +4,54 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    private HashSet<GameObject> portalObjects = new HashSet<GameObject>();
-    [SerializeField] private Transform destination;
+    private HashSet<GameObject> portalObjects = new HashSet<GameObject>(); 
+    [SerializeField] private Transform destination; 
+    private bool isActivated = false; 
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    
+    public void TriggerPortal(Collider2D collision)
     {
+        
+        if (!isActivated)
+        {
+            Debug.Log("Portal is not activated yet.");
+            return;
+        }
+
+        
         if (portalObjects.Contains(collision.gameObject))
         {
             return;
         }
 
+        
         if (destination.TryGetComponent(out Portal destinationPortal))
         {
             destinationPortal.portalObjects.Add(collision.gameObject);
         }
 
+        
         collision.transform.position = destination.position;
     }
 
+    
     private void OnTriggerExit2D(Collider2D collision)
     {
         portalObjects.Remove(collision.gameObject);
+    }
+
+    
+    public void ActivatePortal()
+    {
+        isActivated = true; 
+        Debug.Log("Portal activated by light!");
+        
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        TriggerPortal(collision);
     }
 }
