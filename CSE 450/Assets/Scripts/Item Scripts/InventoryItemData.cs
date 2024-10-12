98200c828a895e385a74cbcb63e201343d57324a
czMikeTan <mike.t@wustl.edu>
2024-10-12 01:14:58 -0500

    10/12 modify bag backend and frontend
    improved backend by automaticly stack items of the same type and will put different item type in different slots (both need to be under maximum stack number). Created UI for the bag, and will automaticly display the items in the backend

--- CSE 450/Assets/Scripts/Item Scripts/InventoryItemData.cs
+++ CSE 450/Assets/Scripts/Item Scripts/InventoryItemData.cs
@@ -11,6 +11,6 @@ public class InventoryItemData : ScriptableObject
     public string DisplayName;
     [TextArea(4, 4)]
     public string Description;
-    public Sprite Icon1;
+    public Sprite Icon;
     public int MaxStackSize;
 }
