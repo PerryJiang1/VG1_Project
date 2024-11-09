6f72e1d72bee4c34b8540ee40bdeaa1b984bd3e5
Harry77-Li <haoran.l@wustl.edu>
2024-11-08 19:21:50 -0600

    Funny thing

--- CSE 450/Assets/Code/Player/RobotController.cs
+++ CSE 450/Assets/Code/Player/RobotController.cs
@@ -41,7 +41,8 @@ namespace CharacterMovement
 
         private void FixedUpdate()
         {
-            if (isControlEnabled) {
+            if (isControlEnabled)
+            {
                 float targetSpeed = speed * speedMultiplier;
                 rb.velocity = new Vector2(targetSpeed, rb.velocity.y);
 
@@ -55,7 +56,6 @@ namespace CharacterMovement
                 {
                     rb.velocity += Vector2.up * Physics2D.gravity.y * (riseMultiplier - 1) * Time.fixedDeltaTime;
                 }
-
 
                 if (speedMultiplier == 1 && Time.time - lastLeftPressorLeaseTime < delta)
                 {
@@ -72,65 +72,48 @@ namespace CharacterMovement
             {
                 rb.velocity = Vector2.zero;
             }
-                
         }
-
 
         public void MoveRight(InputAction.CallbackContext value)
         {
-            // Don't process input when time out
-            if (!isControlEnabled)
-            {
-                return;
-            }
+            if (!isControlEnabled) return;
 
-            if (isControlEnabled)
+            if (value.started)
             {
-                if (value.started)
+                buttonPressed = true;
+                lastRightPressorLeaseTime = Time.time;
+                speedMultiplier = 1;
+                spriteRenderer.flipX = false;
+            }
+            else if (value.canceled)
+            {
+                buttonPressed = false;
+                lastRightPressorLeaseTime = Time.time;
+                if (Time.time - lastLeftPressorLeaseTime > delta)
                 {
-                    buttonPressed = true;
-                    lastRightPressorLeaseTime = Time.time;
-                    speedMultiplier = 1;
-                    spriteRenderer.flipX = false;
-                }
-                else if (value.canceled)
-                {
-                    buttonPressed = false;
-                    lastRightPressorLeaseTime = Time.time;
-                    if (Time.time - lastLeftPressorLeaseTime > delta)
-                    {
-                        speedMultiplier = 0;
-                    }
+                    speedMultiplier = 0;
                 }
             }
-                
         }
 
         public void MoveLeft(InputAction.CallbackContext value)
         {
-            // Don't process input when time out
-            if (!isControlEnabled)
-            {
-                return;
-            }
+            if (!isControlEnabled) return;
 
-            if (isControlEnabled)
+            if (value.started)
             {
-                if (value.started)
+                buttonPressed = true;
+                lastLeftPressorLeaseTime = Time.time;
+                speedMultiplier = -1;
+                spriteRenderer.flipX = true;
+            }
+            else if (value.canceled)
+            {
+                buttonPressed = false;
+                lastLeftPressorLeaseTime = Time.time;
+                if (Time.time - lastRightPressorLeaseTime > delta)
                 {
-                    buttonPressed = true;
-                    lastLeftPressorLeaseTime = Time.time;
-                    speedMultiplier = -1;
-                    spriteRenderer.flipX = true;
-                }
-                else if (value.canceled)
-                {
-                    buttonPressed = false;
-                    lastLeftPressorLeaseTime = Time.time;
-                    if (Time.time - lastRightPressorLeaseTime > delta)
-                    {
-                        speedMultiplier = 0;
-                    }
+                    speedMultiplier = 0;
                 }
             }
         }
@@ -140,6 +123,25 @@ namespace CharacterMovement
             if (value.started && onGround)
             {
                 rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+
+        // 添加新功能：按下B键增大角色，按下N键缩小角色
+        private void Update()
+        {
+            // 仅在控制启用时检测按键
+            if (isControlEnabled)
+            {
+                if (Keyboard.current.bKey.wasPressedThisFrame)
+                {
+                    transform.localScale *= 2; // 大小增加两倍
+                    jumpForce *= 2; // 跳跃高度增加两倍
+                }
+                else if (Keyboard.current.nKey.wasPressedThisFrame)
+                {
+                    transform.localScale *= 0.5f; // 大小缩小两倍
+                    jumpForce *= 0.5f; // 跳跃高度减少两倍
+                }
             }
         }
 
