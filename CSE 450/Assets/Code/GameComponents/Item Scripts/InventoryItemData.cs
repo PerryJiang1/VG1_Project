9ae80da9df5fa8af7d2fa6e45d4af5da8a695847
jirrrannn <cindyzhang175@gmail.com>
2024-11-17 20:36:21 -0600

    upload

--- CSE 450/Assets/Code/GameComponents/Item Scripts/InventoryItemData.cs
+++ CSE 450/Assets/Code/GameComponents/Item Scripts/InventoryItemData.cs
@@ -11,6 +11,6 @@ public class InventoryItemData : ScriptableObject
     public string DisplayName;
     [TextArea(4, 4)]
     public string Description;
-    public Sprite Icon;
+    public Sprite Icon1;
     public int MaxStackSize;
 }
